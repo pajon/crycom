8128601af23affcf183bc35b962da1b47ca93691
Patrik Štrba <patrik.strba@gmail.com>
2013-10-17 14:03:04 +0200

    init

---
+++ app.js
@@ -0,0 +1,35 @@
+var WebSocketServer = require('ws').Server;
+var packet = require('./lib/packet');
+var controller = require('./lib/controller');
+var client = require('./lib/client');
+var db = require('./lib/database');
+
+db.connect();
+
+var wss = new WebSocketServer({port: 8000})
+
+wss.on('connection', function (ws) {
+    ws.client = new Client(ws);
+
+    ws.on('error', function (error) {
+        console.log('error: %s', error);
+    });
+
+    ws.on('message', function (message, flags) {
+        var p;
+        if (flags.binary === undefined) {
+            p = new Packet();
+            p.fromJson(message.toString());
+        } else {
+            p = new Packet(message);
+        }
+        controller.route(ws.client, p);
+    });
+
+    ws.on('close', function (code, message) {
+        if (ws.client.isLogged()) {
+            ws.client.disconnect();
+        }
+
+    });
+});
