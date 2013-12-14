ced07c35cfb76a519f270f9627521ae9ae23d617
Patrik Štrba <patrik.strba@gmail.com>
2013-12-14 14:01:32 +0100

    fix domain name

--- app.js
+++ app.js
@@ -11,13 +11,26 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 8000;
 
-app.get('/*', function(req, res, next) {
-    if (req.headers.host.match(/^www/) !== null ) {
-        res.redirect('http://' + req.headers.host.replace(/^www\./, '') + req.url);
+app.use(function(req, res, next){
+    var hostarr = req.headers.host.split(":");
+    var host = null;
+    if(typeof hostarr === 'object') {
+        host = hostarr[0];
+    } else
+        host = hostarr;
+    
+
+    if (host.match(/^app.crycom.(loc|net)$/) === null) {
+        if(host.match(/crycom.(loc|net)$/) === null)
+            res.redirect('http://app.crycom.net' + req.url);
+        else {
+            var dname = host.split(".");
+            res.redirect('http://app.crycom.' + dname[dname.length - 1] + req.url);
+        }
     } else {
         next();
     }
-})
+});
 
 app.use(express.static(__dirname + '/static'));
 
