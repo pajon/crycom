becfcd36223d06da39e0e5ca8ab7283206cb70ed
Patrik Štrba <patrik.strba@gmail.com>
2013-12-22 11:51:36 +0100

    add compiling javascripts

---
+++ compile.js
@@ -0,0 +1,10 @@
+({
+    baseUrl: "./static/js",
+    //paths: {
+    //    jquery: "some/other/jquery"
+    //},
+    preserveLicenseComments: false,
+    mainConfigFile: 'static/js/bootstrap.js',
+    name: "bootstrap",
+    out: "static/js/compile.js"
+})
--- static/js/extern.js
+++ static/js/extern.js
@@ -1,20 +1,20 @@
 define([
-    //'extern/jsbn',
-    //'extern/jsbn2',
-    //'extern/prng4',
-    //'extern/rng',
-    //'extern/rsa',
-    //'extern/aes',
-    //'extern/asn1',
+    'extern/jsbn',
+    'extern/jsbn2',
+    'extern/prng4',
+    'extern/rng',
+    'extern/rsa',
+    'extern/aes',
+    'extern/asn1',
+    'extern/rsa2',
+    'extern/rsa-sign',
+    'extern/base64',
+    'extern/pem',
     'extern/bootstrap',
     'extern/jquery.json',
     'extern/jstorage',
     'extern/notify',
     'extern/sha256',
-    'extern/spin',
-    //'extern/rsa2',
-    //'extern/rsa-sign',
-    //'extern/base64',
-    //'extern/pem'
+    'extern/spin'
 
 ], function () {});
